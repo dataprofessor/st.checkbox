f8912739204c895988ed456e6c3818607852e38f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-04-06 10:37:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,13 @@ import streamlit as st
 
 st.header('st.checkbox')
 
-st.write ('Do you like ice cream?')
+st.write ('Do you want ice cream?')
 
 yes = st.checkbox('Yes')
+no = st.checkbox('No')
 
 if yes:
      st.write("Great! Here's some more 🍦")
+     
+if no: 
+     st.write("Okay, here's some coffee ☕")
